d55efc231d9506b5238329c2fcfd9dcbf8535770
vinter <ankasandora@gmail.com>
2019-09-08 11:49:29 +0900

    add: pprof

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -21,6 +21,7 @@ import (
 	goji "goji.io"
 	"goji.io/pat"
 	"golang.org/x/crypto/bcrypt"
+	_ "net/http/pprof"
 )
 
 const (
@@ -279,6 +280,9 @@ func init() {
 }
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	host := os.Getenv("MYSQL_HOST")
 	if host == "" {
 		host = "127.0.0.1"
