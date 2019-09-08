085c1bf12309c24572a6242e1d2e541b9de24b2a
tiwanari <tatsuyaw0c@gmail.com>
2019-09-08 14:43:12 +0900

    Fix Printf bugs

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -538,7 +538,7 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 
 	itemSimples := []ItemSimple{}
 	if itemID > 0 && createdAt > 0 {
-		log.Printf("paging %d %s", itemID, createdAt)
+		log.Printf("paging %d %d", itemID, createdAt)
 		// paging
 		query := `
 SELECT
@@ -574,7 +574,7 @@ ORDER BY t1.created_at DESC, t1.id DESC LIMIT ?
 			return
 		}
 	} else {
-		log.Printf("1st page %d %s", itemID, createdAt)
+		log.Printf("1st page %d %d", itemID, createdAt)
 		// 1st page
 		query := `
 SELECT
