fc618fe36b41d6a2f75254db1b02c792497bd1c7
vinter <ankasandora@gmail.com>
2019-09-08 17:13:42 +0900

    change index

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -32,8 +32,8 @@ CREATE TABLE `items` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_category_id (`category_id`, `created_at`),
-  INDEX idx_seller_id (`seller_id`, `created_at`),
-  INDEX idx_buyer_id (`buyer_id`, `created_at`)
+  INDEX idx_seller_buyer_id (`seller_id`, `buyer_id`, `created_at`),
+  INDEX idx_created_at (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
